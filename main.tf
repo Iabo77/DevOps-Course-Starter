4bc735f26eefe00d1a5f81b96e1b3f2cf9bb37f9
Ian B <94004061+Iabo77@users.noreply.github.com>
2022-11-03 18:05:41 +0000

    initial main.tf running plan without error

--- main.tf
+++ main.tf
@@ -14,3 +14,74 @@ provider "azurerm" {
 data "azurerm_resource_group" "main" {
   name = "OpenCohort21_IanBoorer_ProjectExercise"
 }
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "terraformed-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "main" {
+  name                = "todoapp-ib-tf"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.main.id
+
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "DOCKER|appsvcsample/python-helloworld:latest"
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+    "MONGODB_CONNECTION_STRING" = azurerm_cosmosdb_account.main.connection_strings[0]
+    }
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_cosmosdb_account" "main" {
+  name                = "ibtftodo-mongodb-${random_integer.ri.result}"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+  enable_automatic_failover = false
+
+  capabilities {
+    name = "EnableServerless"
+  }  
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "Session"
+    max_interval_in_seconds = 5
+    max_staleness_prefix    = 100
+  }
+
+  geo_location {
+    location          = "UK West"
+    failover_priority = 0
+  }
+}
+
+
+resource "azurerm_cosmosdb_mongo_database" "main" {
+  name                = "todoapp-db"
+  resource_group_name = azurerm_cosmosdb_account.main.resource_group_name
+  account_name        = azurerm_cosmosdb_account.main.name
+  throughput          = 400
+}
