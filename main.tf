da6e4a5bb9987b210682e40d7d011b39db4876a6
Ian B <94004061+Iabo77@users.noreply.github.com>
2022-11-07 16:28:34 +0000

    terraform deploys (with local state. to part 2)

--- main.tf
+++ main.tf
@@ -15,43 +15,50 @@ data "azurerm_resource_group" "main" {
   name = "OpenCohort21_IanBoorer_ProjectExercise"
 }
 
-resource "azurerm_app_service_plan" "main" {
-  name                = "terraformed-asp"
+resource "azurerm_service_plan" "main" { 
+  name                = "${var.prefix}-terraformed-asp"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-}
-
-resource "azurerm_app_service" "main" {
-  name                = "todoapp-ib-tf"
+  os_type             = "Linux" 
+  sku_name            = "B1" 
+} 
+ 
+resource "azurerm_linux_web_app" "main" { 
+  name                = "${var.prefix}-todoapp"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  app_service_plan_id = azurerm_app_service_plan.main.id
-
+  service_plan_id     = azurerm_service_plan.main.id
+  
   site_config {
-    app_command_line = ""
-    linux_fx_version = "DOCKER|appsvcsample/python-helloworld:latest"
+    application_stack {
+      docker_image     = "${var.DOCKER_IMAGE}"
+      docker_image_tag = "latest"
+    }
   }
 
-  app_settings = {
-    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
-    "MONGODB_CONNECTION_STRING" = azurerm_cosmosdb_account.main.connection_strings[0]
+  app_settings = {    
+    DOCKER_REGISTRY_SERVER_URL = "https://index.docker.io"
+    DOCKER_REGISTRY_SERVER_USERNAME = "${var.DOCKER_REGISTRY_SERVER_USERNAME}"
+    DOCKER_REGISTRY_SERVER_PASSWORD = "${var.DOCKER_REGISTRY_SERVER_PASSWORD}"   
+
+    CLIENT_ID= "${var.CLIENT_ID}" 
+    CLIENT_SECRET="${var.CLIENT_SECRET}"
+    GITHUB_OAUTH_URL="https://github.com/login/oauth/authorize"
+    REDIRECT_URI="https://${var.prefix}-todoapp.azurewebsites.net/login/callback"  
+
+    CONNECTION_STRING=azurerm_cosmosdb_account.main.connection_strings[0]
+    DATABASE="todo_app"
+    COLLECTION="items"
+
+    SECRET_KEY="${var.SECRET_KEY}"
+    FLASK_APP="${var.FLASK_APP}"
+    FLASK_ENV="${var.FLASK_ENV}"
     }
 }
 
-resource "random_integer" "ri" {
-  min = 10000
-  max = 99999
-}
 
 resource "azurerm_cosmosdb_account" "main" {
-  name                = "ibtftodo-mongodb-${random_integer.ri.result}"
+  name                = "${var.prefix}-mongodb-account"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   offer_type          = "Standard"
@@ -66,6 +73,10 @@ resource "azurerm_cosmosdb_account" "main" {
     name = "EnableMongo"
   }
 
+    lifecycle { 
+    prevent_destroy = true 
+  }
+  
   consistency_policy {
     consistency_level       = "Session"
     max_interval_in_seconds = 5
@@ -80,8 +91,7 @@ resource "azurerm_cosmosdb_account" "main" {
 
 
 resource "azurerm_cosmosdb_mongo_database" "main" {
-  name                = "todoapp-db"
+  name                = "${var.prefix}-todoapp-db"
   resource_group_name = azurerm_cosmosdb_account.main.resource_group_name
   account_name        = azurerm_cosmosdb_account.main.name
-  throughput          = 400
 }
---
+++ variables.tf
@@ -0,0 +1,80 @@
+variable "prefix" {
+    description = "The prefix used for all resources in this environment"
+    default = "ibtf"
+    }
+
+variable "CLIENT_ID" {
+    type = string
+    sensitive = true
+}
+
+variable "CLIENT_SECRET" {
+    type = string
+    sensitive = true
+
+}
+
+variable "GITHUB_OAUTH_URL" {
+    type = string
+    sensitive = true
+
+}
+
+
+variable "COLLECTION" {
+    type = string
+    sensitive = true
+}
+
+variable "DATABASE" {
+    type = string
+    sensitive = true
+
+}
+
+variable "SECRET_KEY" {
+    type = string
+    sensitive = true
+
+}
+
+variable "FLASK_APP" {
+    type = string
+    sensitive = true
+
+}
+
+variable "FLASK_ENV" {
+    type = string
+    sensitive = true
+
+}
+
+variable "DOCKER_IMAGE" {
+    type = string
+    sensitive = true
+
+}
+
+variable "DOCKER_REGISTRY_SERVER_URL" {
+    type = string
+    sensitive = true
+
+}
+
+variable "DOCKER_REGISTRY_SERVER_USERNAME" {
+    type = string
+    sensitive = true
+}
+
+
+variable "DOCKER_REGISTRY_SERVER_PASSWORD" {
+    type = string
+    sensitive = true
+}
+
+
+variable "location" {
+  description = "The Azure location where all resources in this deployment should be created"
+  default     = "uksouth"
+}
