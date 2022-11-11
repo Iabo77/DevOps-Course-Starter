bfc66c11e3e179d49bf0562c9713bdb557e1707e
Ian B <94004061+Iabo77@users.noreply.github.com>
2022-11-11 11:54:46 +0000

    updated terraform main with azure authentication

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.92"
+      version = "=3.0.0"
     }
   }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
    backend "azurerm" {
         resource_group_name  = "OpenCohort21_IanBoorer_ProjectExercise"
         storage_account_name = "tfstate10ibtf"
